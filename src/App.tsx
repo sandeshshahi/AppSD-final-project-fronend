6d33f04fbdc5b7191876e4167405f8c91f25a778
Sandesh Shahi <SandeshShahi@Sandeshs-MacBook-Pro.local>
2026-04-23 02:11:48 -0500

    chore(frontend): bootstrap app shell and project dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,101 @@
-function App() {
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ProtectedRoute } from "./routes/ProtectedRoute";
+import { LoginPage } from "./features/auth/pages/LoginPage";
+import { DashboardLayout } from "./layouts/DashboardLayout";
+import { InvoicesPage } from "./features/billing/pages/InvoicesPage";
+import { BookAppointmentPage } from "./features/appointments/pages/BookAppointmentPage";
+import { XRayUploadPage } from "./features/dentists/pages/XRayUploadPage";
+import { PatientListPage } from "./features/dentists/pages/PatientListPage";
+import { ManagerDashboard } from "./features/analytics/pages/ManagerDashboard";
+import { StaffManagementPage } from "./features/dentists/pages/StaffManagementPage";
+import { SignupPage } from "./features/auth/pages/SignupPage";
+import { MyRecordsPage } from "./features/patients/pages/MyRecordsPage";
+import { SurgeryManagementPage } from "./features/surgery/pages/SurgeryManagementPage";
+import { Toaster } from "react-hot-toast";
+import { HomePage } from "./pages/HomePage";
+import { ProfilePage } from "./features/auth/pages/ProfilePage";
+import { DashboardRouter } from "./routes/DashboardRouter";
+import { AdminPatientListPage } from "./features/analytics/pages/AdminPatientListPage";
+import { AdminBillingPage } from "./features/billing/pages/AdminBillingPage";
+import { AdminAppointmentsPage } from "./features/appointments/pages/AdminAppointmentsPage";
+
+export default function App() {
   return (
-    <>
-      <h1 className="text-red-400">hello</h1>
-    </>
+    <BrowserRouter>
+      <Toaster position="top-right" reverseOrder={false} />
+
+      <Routes>
+        {/*  Public Landing Page */}
+        <Route path="/" element={<HomePage />} />
+
+        {/*  Public Auth Routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+
+        {/*  Authenticated Wrapper */}
+        <Route element={<ProtectedRoute />}>
+          {/*  Layout Wrapper (Sidebar & Header) */}
+          <Route element={<DashboardLayout />}>
+            {/*  SHARED Authenticated ROUTES   */}
+            <Route path="/dashboard" element={<DashboardRouter />} />
+            <Route path="/profile" element={<ProfilePage />} />
+
+            {/*  PATIENT ONLY ROUTES  */}
+            <Route element={<ProtectedRoute allowedRoles={["PATIENT"]} />}>
+              <Route path="/invoices" element={<InvoicesPage />} />
+              <Route
+                path="/book-appointment"
+                element={<BookAppointmentPage />}
+              />
+              <Route path="/my-records" element={<MyRecordsPage />} />
+            </Route>
+
+            {/* OFFICE MANAGER ONLY ROUTES */}
+            <Route
+              element={<ProtectedRoute allowedRoles={["OFFICE_MANAGER"]} />}
+            >
+              <Route path="/admin/stats" element={<ManagerDashboard />} />
+              <Route path="/admin/staff" element={<StaffManagementPage />} />
+              <Route
+                path="/admin/surgeries"
+                element={<SurgeryManagementPage />}
+              />
+              <Route
+                path="/admin/appointments"
+                element={<AdminAppointmentsPage />}
+              />
+
+              <Route
+                path="/admin/patients"
+                element={<AdminPatientListPage />}
+              />
+              <Route path="/admin/billing" element={<AdminBillingPage />} />
+            </Route>
+
+            {/*  DENTIST ONLY ROUTES */}
+            <Route element={<ProtectedRoute allowedRoles={["DENTIST"]} />}>
+              <Route path="/patients" element={<PatientListPage />} />
+              <Route
+                path="/upload-xray/:patientId"
+                element={<XRayUploadPage />}
+              />
+            </Route>
+          </Route>
+        </Route>
+
+        {/* Fallbacks */}
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        <Route
+          path="/unauthorized"
+          element={<div className="p-10 text-center">Access Denied</div>}
+        />
+        <Route
+          path="*"
+          element={
+            <div className="p-10 text-center text-2xl">404: Not Found</div>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
+import { ApolloProvider } from "@apollo/client/react";
+import { client } from "./graphql/client.ts";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </AuthProvider>
   </StrictMode>,
-)
+);
